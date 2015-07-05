87f4e461ae0a843793691265f6ad6f304ce966d8
Andre Baltieri <andrebaltieri@hotmail.com>
2015-07-05 12:54:19 -0300

    iniciado repo

---
+++ Assets/Scripts/Bird.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Bird : MonoBehaviour {
+	// Movement speed
+	public float speed = 2;
+	
+	// Flap force
+	public float force = 300;
+	
+	// Use this for initialization
+	void Start () {    
+		// Fly towards the right
+		GetComponent<Rigidbody2D>().velocity = Vector2.right * speed;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		// Flap
+		if (Input.touchCount > 0 && Input.GetTouch(0).phase == TouchPhase.Moved)
+			GetComponent<Rigidbody2D>().AddForce(Vector2.up * force);
+
+		if(Input.GetMouseButtonDown(0))
+			GetComponent<Rigidbody2D>().AddForce(Vector2.up * force);
+	}
+
+	void OnCollisionEnter2D(Collision2D coll) {
+		// Restart
+		Application.LoadLevel(Application.loadedLevel);
+	}
+}
